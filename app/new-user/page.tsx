1c041e93045c85ef6f3fde577d60bcf07e875109
RahafTahayneh <rahaf.tahayneh@yembo.ai>
2023-09-11 15:41:27 +0300

    push database initilaizations and models

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,4 +1,32 @@
-const NewUser = () => {
+import { prisma } from "@/utils/db";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const createNewUser = async () => {
+  const user = await currentUser();
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  });
+
+  if (!match && user) {
+    await prisma.user.create({
+      data: {
+        firstName: user?.firstName,
+        lastName: user?.lastName,
+        clerkId: user?.id,
+        email: user?.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect("/journal");
+};
+
+const NewUser = async () => {
+  await createNewUser();
   return <div>...loading</div>;
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
 export default async function Home() {
+  const { userId } = await auth();
+  const href = userId ? "/journal" : "/new-user";
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -12,7 +14,7 @@ export default async function Home() {
           .
         </p>
         <div>
-          <Link href={"/sign-in"}>
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
